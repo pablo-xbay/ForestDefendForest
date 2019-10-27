6527dc96f2922fff8728adacec5b18c8ba8c118d
Pavel Tiguntsev <pavel@xbay.io>
2019-10-27 05:30:58 -0700

    CommitToTheForest

---
+++ src/js/actors/Actor.js
@@ -0,0 +1,51 @@
+import config from '../config.js'
+
+export default class Actor {
+  constructor () {
+    this._sprite = null
+    this.height = 0
+    this.width = 0
+  }
+
+  set sprite (name) {
+    this.height = config.sprites[name].h /2 
+    this._sprite = name
+    this.width = config.sprites[name].w / 2
+  }
+
+  get sprite () {
+    return this._sprite
+  }
+
+  hits (actors) {
+    return actors
+      .filter(Boolean)
+      .some(actor => {
+        if (this.x >= (actor.x + actor.width-6) || actor.x >= (this.x + this.width-6)) {
+          return false
+        }
+
+        if (this.y >= (actor.y + actor.height-6) || actor.y >= (this.y + this.height-6)) {
+          return false
+        }
+
+        return true
+      })
+  }
+
+  fhits (actors) {
+    return actors
+      .filter(Boolean)
+      .some(actor => {
+        if (this.x - 6 >= (actor.x + actor.width-6 + 35) || actor.x - 6 >= (this.x + this.width-6  + 35)) {
+          return false
+        }
+
+        if (this.y >= (actor.y + actor.height-6) || actor.y >= (this.y + this.height-6)) {
+          return false
+        }
+
+        return true
+      })
+  }
+}
